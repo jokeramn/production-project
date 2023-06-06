8c974371f7f94acdd4bd642c71be16a574f14ec8
Sergey Prokopcuk <seraapasik@gmail.com>
2023-06-06 17:34:16 +0300

    added babel and extractPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,7 +3,9 @@ import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HtmlWebpackPlugin({
             template: paths.html
@@ -12,6 +14,10 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
-        })
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev)
+        }),
+        ...(isDev ? [new ReactRefreshWebpackPlugin()] : [])
     ]
 }
