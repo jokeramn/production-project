cfe64a049ab86d068ee6a2c92ee91a5b8a124b0b
Sergey Prokopcuk <seraapasik@gmail.com>
2023-06-14 16:48:23 +0300

    add jest, add first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,25 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first params', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        expect(classNames('someClass', {}, ['hello'])).toBe('someClass hello');
+    });
+    test('with all params', () => {
+        const expected = 'someClass hello1 hello2 kek';
+
+        expect(classNames('someClass', { kek: true }, ['hello1', 'hello2'])).toBe(expected);
+    });
+    test('with mods false', () => {
+        const expected = 'someClass hello1 hello2';
+
+        expect(classNames('someClass', { kek: false }, ['hello1', 'hello2'])).toBe(expected);
+    });
+    test('with mods undefined', () => {
+        const expected = 'someClass hello1 hello2';
+
+        expect(classNames('someClass', { kek: undefined }, ['hello1', 'hello2'])).toBe(expected);
+    });
+});
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,8 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.keys(mods)
-            .filter((key) => mods[key]),
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
     ].join(' ');
 }
